9569ffa35f008ebc9d6a03751f7d27b5c156dfe9
Bill Schofield <bill@billschofield.net>
2013-08-18 16:08:29 -0500

    Added game loop that draws a Moire pattern.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 var canvas = document.getElementById('gameOne');
 var context = canvas.getContext('2d');
+var x = 0;
+var y = 200;
+(function (window) {
+    function gameLoop() {
+        context.beginPath();
+        context.moveTo(0, 0);
+        context.lineTo(x++, y--);
+        context.stroke();
+        x++;
+    }
+    window.setInterval(gameLoop, 1000 / 60); // 60fps
+} (window));
 
-context.beginPath();
-context.moveTo(0, 0);
-context.lineTo(400, 200);
-context.stroke();
+
