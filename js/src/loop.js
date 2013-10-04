f589e45e755e9404cf960c89e3be05f738c51f20
Bill Schofield <bill@billschofield.net>
2013-10-04 10:37:22 -0500

    Extracted gameLoop concept from runner

---
+++ js/src/loop.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var game = game || {};
+
+game.loop = function(spec){
+    var that = {};
+    var context = spec.context;
+    var path = spec.path;
+    var entity = spec.entity;
+
+    that.run = function(window) {
+            function gameLoop() {
+                context.clearRect(0, 0, 800, 200);
+                path.draw();
+                entity.update();
+                entity.draw();
+            }
+
+            var framesPerSecond = 1000 / 60;
+            window.setInterval(gameLoop, framesPerSecond);
+    };
+
+    return that;
+};
--- js/src/runner.js
+++ js/src/runner.js
@@ -1,20 +1,6 @@
 var game = game || {};
 
 game.runner = function(spec){
-    function gameLoopRunner() {
-        return function (window) {
-            function gameLoop() {
-                context.clearRect(0, 0, 800, 200);
-                path.draw();
-                entity.update();
-                entity.draw();
-            }
-
-            var framesPerSecond = 1000 / 60;
-            window.setInterval(gameLoop, framesPerSecond);
-        };
-    }
-
     var that = {};
     var document = spec.document;
     var canvas = document.getElementById('gameOne');
@@ -22,7 +8,7 @@ game.runner = function(spec){
     var entity = game.entityFactory({context:context}).create();
     var path = game.path({context: context});
 
-    that.go = gameLoopRunner();
+    that.go = game.loop({context:context, path:path, entity:entity}).run;
     $(document.body).on('keydown', game.keyPressEventHandler({entity:entity}).handle);
     canvas.addEventListener('click', game.clickEventHandler({canvas:canvas, path:path}).handle, false);
     return that;
